745f6a9f0ca354310c5c5a22114085b8ed6934fa
Gabriel <gab.patterson@gmail.com>
2019-04-07 15:06:50 -0400

    initial commit

--- norm1/ViewController.swift
+++ norm1/ViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+
+    @IBOutlet weak var topTextField: UITextField!
+     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var bottomTextField: UITextField!
+    //struct to hold image & text info
+    struct Meme{
+        var originalImage:UIImage?
+        var memedImage:UIImage?
+        var topText:String=""
+        var bottomText:String=""
+    }
+    //not complete function to change meme struct values
+    func generateMemedImage() -> UIImage {
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return memedImage
+    }
+  
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +41,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func pickView(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate=self
+        pickerController.sourceType = .savedPhotosAlbum
+        self.present(pickerController, animated: true, completion: nil)
+        
+            }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imagePickerView.image = image
+      
+        dismiss(animated:true, completion: nil)
+    }
+    
 }
-

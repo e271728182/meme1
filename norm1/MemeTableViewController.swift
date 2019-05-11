41881c3307711ced3e097f532d3c355a1ee65db6
Gabriel <gab.patterson@gmail.com>
2019-05-11 16:04:56 -0300

    code cleanup

--- norm1/MemeTableViewController.swift
+++ norm1/MemeTableViewController.swift
@@ -12,20 +12,19 @@ class MemeTableViewController: UITableViewController {
 
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
+   
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         self.tableView.reloadData()
     }
+   
     @IBAction func goToMemeViewController(_ sender: AnyObject) {
-        
         let detailController = self.storyboard!.instantiateViewController(withIdentifier: "MemeController") as! MemeViewController
         self.navigationController!.pushViewController(detailController, animated: true)}
     
     // MARK: Table View Data Source
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-    print("table",appDelegate.memes.count)
         return appDelegate.memes.count
     
     }
@@ -33,15 +32,14 @@ class MemeTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell")!
-
         // Set the name and image
         cell.textLabel?.text = appDelegate.memes[indexPath.row].topText + appDelegate.memes[indexPath.row].bottomText
         cell.imageView?.image = appDelegate.memes[indexPath.row].originalImage
     return cell
+        
     }
+    
     //present the controller and start the app as if it was Meme1.0
-
-
     override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         let detailController = self.storyboard!.instantiateViewController(withIdentifier: "OldMeme") as! OldMemeViewController
         detailController.imDUmp = appDelegate.memes[indexPath.row].memedImage
